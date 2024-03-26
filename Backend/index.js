8ad0362d799d82d978ae98aedd37afe114e12c3b
n_sam <n_sammacheep@outlook.com>
2024-03-25 21:13:37 -0400

    Added code for database query

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,58 @@
 // creates connections to express, mySQL, and cors
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
 
 const app = express();
-app.use(cors());
+app.use(express.json())
+app.use(cors({
+    origin: [],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 // connecting to the mySQL database
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "Pharmafriend" 
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 })
 
+// lets the user register and stores in the database
+app.post('/register', (req, res) => {
+    const{email, password} = req.body;
 
-// returns to the page
-app.get('/', (re, res)=> {
-    return res.json("From Backend side");
-})
+    db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (err, result) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send("Error registering new user");
+        } else {
+            res.send("User registered successfully!");
+        }
+    });
+});
 
-// gets anything written to translate to the page
-app.listen(8081, ()=> {
-    console.log("listening");
+// lets the user add medication and stores it in the database
+app.post('/add_Medication', (req, res) => {
+    const{prescriptionName, timesPerDay, startDate, endDate} = req.body;
+
+    db.query('INSERT INTO medicineinfo (prescriptionName, timesPerDay, startDate, endDate) VALUES (?, ?, ?, ?, ?)',
+    [prescriptionName, timesPerDay, startDate, endDate], (err, result) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send("Error adding medication");
+        } else {
+            res.send("Medication added successfully!");
+        }
+    });
+});
+
+
+
+// updates the app
+app.listen(process.env.PORT, ()=> {
+    console.log("listening on port", process.env.PORT);
 })
