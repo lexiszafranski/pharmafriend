9d1d68876bb68e49bbc1b4b0ed97aff3a28bd55b
lexiszafranski <aszafranski@ufl.edu>
2024-04-12 00:26:47 -0400

    FAQ page reorganized a bit

--- Frontend/src/faq.js
+++ Frontend/src/faq.js
@@ -11,32 +11,32 @@ const FAQPage = () => {
   return (
     <>
       <header className="jumbotron text-center">
-        <button onClick={handleLogin} style={{ position: 'absolute', top: 20, right: 80 }}>Login</button>
-        <h1 style={{ textAlign: 'center', fontSize: '2rem', color: '#333', marginBottom: '40px', fontFamily: "'Trykker', serif" }}>Frequently Asked Questions</h1>
+        <button className="button-54" onClick={handleLogin} style={{ position: 'absolute', top: 20, right: 80 }}>Login</button>
+        <h1 style={{ textAlign: 'center', fontSize: '2rem', color: 'black', marginBottom: '10px', fontFamily: "'Work Sans', sans-serif" }}>F A Q S</h1>
         <p style={{ textAlign: 'center', fontSize: '1rem' }}>Need help? We got you covered!</p>
       </header>
       <section className="container">
         <div className="row align-items-start">
-          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: 'lavender' }}>
+          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: '#D1D8DE' }}>
             <h2>What is Pharmafriend?</h2>
             <p>Pharmafriend is a medication reminder app that uses a virtual pet to gamify the experience of taking medications and incentivize users to be consistent with their medication schedules.</p>
           </div>
-          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: '#48C6F9' }}>
+          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: 'lavender' }}>
             <h2>How do I use Pharmafriend?</h2>
             <p>Navigate to the login page and create an account. Then click the Add Medication button to log your medication. To feed your pet click the Log Medication button, and watch your pet grow!</p>
           </div>
-          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: 'lavender' }}>
+          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: '#D1D8DE' }}>
             <h2>How much does Pharmafriend cost?</h2>
             <p>Pharmafriend is a free service!</p>
           </div>
-          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: '#48C6F9' }}>
+          <div className="col-md-12 text-left feature-box questions" style={{ backgroundColor: 'lavender' }}>
             <h2>Is my information safe with Pharmafriend?</h2>
             <p>Yes! Your information is kept private and secure. If you decide to leave Pharmafriend, your information will be deleted.</p>
           </div>
         </div>
       </section>
       <footer className="text-center mt-4">
-        <p>By Lexi, Napat, Wyatt, and Imran</p>
+        <p style={{textAlign: 'center'}}>By Lexi, Napat, Wyatt, and Imran</p>
       </footer>
     </>
   );
